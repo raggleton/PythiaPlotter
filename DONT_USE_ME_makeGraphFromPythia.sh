eda3e60dfe30b5a6fafa8099625fab84bb16ac51
Robin Aggleton <robinaggleton@gmail.com>
2014-05-29 10:10:32 +0100

    Some old updates to bash script

--- DONT_USE_ME_makeGraphFromPythia.sh
+++ DONT_USE_ME_makeGraphFromPythia.sh
@@ -3,7 +3,7 @@
 # Script that converts the event listing from Pythia 
 # into a Graphviz file to be plotted with dot
 # e.g. 
-# ./makeGraphFromPythia > myEvent.gv
+# ./makeGraphFromPythia.sh
 # dot -Tpdf myEvent.gv -o myEvent.pdf
 
 # Filename for output graphviz file
@@ -32,91 +32,113 @@ interesting=( "tau+" "tau-" "mu+" "mu-" )
 # To hold all the initial state particles that should be aligned
 sameOnes=""
 
+# To hold all the particles that should be skipped
+skipList=( "" )
+
 echo "digraph g {" > $outputFile
 echo "    rankdir = RL;" >> $outputFile
 for ((i=${#NumNameArr[@]}-1; i>=0; i--))
 # for i in {2..10}
 do
-	# Create string to be written to fle
-	gEntry="    \"${NumNameArr[$i]}\" "
-	
-	finalState=false
-	initialState=false
-	
-	# Check if final-state or not: 
-	# non final state are named "(mu+)", final state are "mu+""
-	if [[ ${NumNameArr[$i]} == *\(* ]]
+	# Check whether in skip list	
+	skip="false"
+	for s in "${skipList[@]}"
+	do
+		if [[ ${NameArr[$i]} == "$s" ]]
+		then
+			skip=true
+		fi
+	done
+
+	# If not in skip list, look at particle entry
+	if [[ $skip == "false" ]]
 	then
+
+		# Create string to be written to fle
+		gEntry="    \"${NumNameArr[$i]}\" "
+		
 		finalState=false
-	else
-		finalState=true
-	fi
+		initialState=false
+		
+		# Check if final-state or not: 
+		# non final state are named "(mu+)", final state are "mu+""
+		if [[ ${NumNameArr[$i]} == *\(* ]]
+		then
+			finalState=false
+		else
+			finalState=true
+		fi
 
-
-	# Check if mother2==0 
-	if [[ ${m2Arr[$i]} -eq 0 ]]
-	then
 		# Check if both mothers are == 0 - then initial state
 		if [[ ${m1Arr[$i]} -eq 0 ]]
 		then
 			initialState=true
 			sameOnes+="\"${NumNameArr[$i]}\" "
+		# Check if mother2==0 & final state
+		elif [[ ${m2Arr[$i]} -eq 0 ]]
+		then
+				m1No=${m1Arr[$i]}
+				mum=${NumNameArr[$m1No]}
+				gEntry+="-> \"$mum\""
+		# Check to see if it is a redundant particle (either m2 == 0, or m1=m2)
+		# elif [[ ${m1Arr[$i]} -eq 0 ]]
+		# then
+		# || (${m1Arr[$i]} -eq ${m2Arr[$i]}) ]]
+			#statements
+			echo "redundant"
 		else
-			m1No=${m1Arr[$i]}
-			mum=${NumNameArr[$m1No]}
-			gEntry+="-> \"$mum\""
+			# Satisfied it isn't a redundant, initial or final-state particle
+
+			# Has mothers from m1 to m2 
+			gEntry+="-> { "
+			m1=${m1Arr[$i]}
+			m2=${m2Arr[$i]}
+			for m in $(eval echo {$m1..$m2})
+			do
+				mum=${NumNameArr[$m]}
+				gEntry+="\"$mum\" "
+			done
+			gEntry+=" }"
 		fi
-	else
-		# Has mothers from m1 to m2 
-		gEntry+="-> { "
-		m1=${m1Arr[$i]}
-		m2=${m2Arr[$i]}
-		for m in $(eval echo {$m1..$m2})
+		
+		# Reverse arrow direction, since we link daughters to mothers
+		gEntry+=" [dir=\"back\"]"
+
+		echo "$gEntry"  >> $outputFile
+		
+		# Check to see if one of the interesting particles we defined above
+		isInteresting=false
+		for p in "${interesting[@]}"
 		do
-			mum=${NumNameArr[$m]}
-			gEntry+="\"$mum\" "
+			if [[ ${NameArr[$i]} == *"$p"* ]]
+			then
+				isInteresting=true
+			fi
 		done
-		gEntry+=" }"
-	fi
-	
-	# Reverse arrow direction, since we link daughters to mothers
-	gEntry+=" [dir=\"back\"]"
 
-	echo "$gEntry"  >> $outputFile
-	
-	# Check to see if one of the interesting particles we defined above
-	isInteresting=false
-	for p in "${interesting[@]}"
-	do
-		if [[ ${NameArr[$i]} == *"$p"* ]]
+		if [[ $isInteresting == "true" ]]
 		then
-			isInteresting=true
-		fi
-	done
-
-	if [[ $isInteresting == "true" ]]
-	then
-		# If final state, make it a square box
-		if [ "$finalState" == "true" ]
-		then
-			echo "        \"${NumNameArr[$i]}\" [label=\"${NumNameArr[$i]}\", shape=box, style=filled, fillcolor=red]"  >> $outputFile
+			# If final state, make it a square box
+			if [ "$finalState" == "true" ]
+			then
+				echo "        \"${NumNameArr[$i]}\" [label=\"${NumNameArr[$i]}\", shape=box, style=filled, fillcolor=red]"  >> $outputFile
+			else
+				echo "        \"${NumNameArr[$i]}\" [label=\"${NumNameArr[$i]}\", style=filled, fillcolor=red]"  >> $outputFile
+			fi	
 		else
-			echo "        \"${NumNameArr[$i]}\" [label=\"${NumNameArr[$i]}\", style=filled, fillcolor=red]"  >> $outputFile
-		fi	
-	else
-		# If initial state, make it a green box
-		if [ "$initialState" == "true" ]
-		then
-			echo "        \"${NumNameArr[$i]}\" [label=\"${NumNameArr[$i]}\", style=filled, fillcolor=green]"  >> $outputFile
-		fi
+			# If initial state, make it a green box
+			if [ "$initialState" == "true" ]
+			then
+				echo "        \"${NumNameArr[$i]}\" [label=\"${NumNameArr[$i]}\", style=filled, fillcolor=green]"  >> $outputFile
+			fi
 
-		# If final state, make it a yellow box
-		if [ "$finalState" == "true" ]
-		then
-			echo "        \"${NumNameArr[$i]}\" [label=\"${NumNameArr[$i]}\", shape=box, style=filled, fillcolor=yellow]"  >> $outputFile
-		fi
-	fi
-
+			# If final state, make it a yellow box
+			if [ "$finalState" == "true" ]
+			then
+				echo "        \"${NumNameArr[$i]}\" [label=\"${NumNameArr[$i]}\", shape=box, style=filled, fillcolor=yellow]"  >> $outputFile
+			fi
+		fi # End of if [[ interesting ]]
+	fi # End of if [[ !skip ]]
 done
 echo "{rank=same;$sameOnes}  // Put them on the same level" >> $outputFile
 echo "}"  >> $outputFile
