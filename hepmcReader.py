3b9e4627f3470e5175e39d0c41a1791fa6e5d14e
Robin Aggleton <robinaggleton@gmail.com>
2014-07-19 16:49:48 +0100

    Add couple of triggers, rename for brevity

--- hepmcReader.py
+++ hepmcReader.py
@@ -4,15 +4,29 @@
     See hepmcformat.txt for reminder, or section 6 of hepMC2 user manual
 """
 
-from eventClasses import Particle
+# from eventClasses import Particle
 
 verbose = True
 
-def parseHepMCFile(fileName):
+def parse(fileName="testSS_HLT.hepmc"):
     """Parse HepMCfile and return collection of events"""
-    
+
     print "Parsing events from ", fileName
     
     with open(fileName, 'r') as file:
+        
         for line in file:
-            if verbose: print line
+            if verbose: print line,
+
+            # Get HepMC version
+            if line.startswith("HepMC::Version"):
+                version = line.split()[1]
+                print "Using HepMC version", version
+
+            # Get start of event block
+            if line.startswith("HepMC::IO_GenEvent-START_EVENT_LISTING"):
+                print "Start parsing event listing"
+
+            # Get end of event block
+            if line.startswith("HepMC::IO_GenEvent-END_EVENT_LISTING"):
+                print "End parsing event listing"
