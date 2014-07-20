c3d1e39ae2bf56451c1520a2205394072c158da1
Robin Aggleton <robinaggleton@gmail.com>
2014-07-20 19:59:54 +0100

    add examples to docstrings

--- hepmcReader.py
+++ hepmcReader.py
@@ -72,19 +72,19 @@ def tidyLineRemoveKey(line, key):
 
 def parseGenEventLine(line):
     """Parse line from HepMC file containting GenEvent info
-    e.g. """
+    e.g. E 0 -1 3.4651814800093540e+01 1.6059648651865022e-01 7.7326991537120665e-03 123 0 707 1 2 0 1 1.0000000000000000e+00 """
     parts = line.split()
 
 
 def parseWeightsLine(line):
     """Parse line from HepMC file containting Weights info
-    e.g."""
+    e.g. N 1 "0" """
     parts = line.split()
 
 
 def parseUnitsLine(line):
     """Parse line from HepMC file containting Units info
-    e.g. U MEV MM"""
+    e.g. U MEV MM """
     parts = line.split()
     units = Units(parts[1], parts[2])
     return units
@@ -92,21 +92,22 @@ def parseUnitsLine(line):
 
 def parseCrossSectionLine(line):
     """Parse line from HepMC file containting CrossSection info
-    e.g. C 1.5299242538371922e+06 4.4721515953955459e+04"""
+    e.g. C 1.5299242538371922e+06 4.4721515953955459e+04 """
     parts = line.split()
 
 
 def parsePdfInfoLine(line):
     """Parse line from HepMC file containting PdfInfo info
-    e.g."""
+    e.g. F 21 21 2.9758908919249000e-03 7.3389857579700624e-02 3.4651814800093540e+01 1.7560069564760610e+01 1.3634687138200170e+00 0 0 """
     parts = line.split()
 
 def parseGenVertexLine(line):
     """Parse line from HepMC file containing GenVertex info
-    e.g."""
+    e.g. V -3 0 0 0 0 0 0 2 0 """
     pass
 
 
 def parseGenParticleLine(line):
-    """Parse line from HepMC file containing GenParticle info"""
+    """Parse line from HepMC file containing GenParticle info
+    e.g. P 4 21 0 0 -2.9355943031880248e+05 2.9355943031880248e+05 0 21 0 0 -3 2 1 102 2 103 """
     pass
