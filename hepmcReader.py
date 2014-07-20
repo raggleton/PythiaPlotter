b069112527856ad608ec8be4ab012a6355a637c7
Robin Aggleton <robinaggleton@gmail.com>
2014-07-20 20:07:22 +0100

    flesh our functions a little, only units and cross section work

--- hepmcReader.py
+++ hepmcReader.py
@@ -58,12 +58,15 @@ def parse(fileName="testSS_HLT.hepmc"):
             if line.startswith("F"):
                 pdfInfo = parsePdfInfoLine(line)
 
+            # Need to deal with repetitive vertex and particle                
+
             # Get end of event block
             if line.startswith("HepMC::IO_GenEvent-END_EVENT_LISTING"):
                 print "End parsing event listing"
 
 
 def tidyLineRemoveKey(line, key):
+    # Do I need this?
     """Tidy up line from file for searching (extra spaces, etc) and remove
     the Line Key (e.g. P, V) from the start to allow further processing"""
 
@@ -74,40 +77,53 @@ def parseGenEventLine(line):
     """Parse line from HepMC file containting GenEvent info
     e.g. E 0 -1 3.4651814800093540e+01 1.6059648651865022e-01 7.7326991537120665e-03 123 0 707 1 2 0 1 1.0000000000000000e+00 """
     parts = line.split()
+    genE = GenEvent()
+    return genE
 
 
 def parseWeightsLine(line):
     """Parse line from HepMC file containting Weights info
     e.g. N 1 "0" """
     parts = line.split()
+    w = Weights()
+    return w
 
 
 def parseUnitsLine(line):
     """Parse line from HepMC file containting Units info
     e.g. U MEV MM """
     parts = line.split()
-    units = Units(parts[1], parts[2])
-    return units
+    u = Units(parts[1], parts[2])
+    return u
 
 
 def parseCrossSectionLine(line):
     """Parse line from HepMC file containting CrossSection info
     e.g. C 1.5299242538371922e+06 4.4721515953955459e+04 """
     parts = line.split()
+    c = GenCrossSection(parts[1], parts[2])
+    return c
 
 
 def parsePdfInfoLine(line):
     """Parse line from HepMC file containting PdfInfo info
     e.g. F 21 21 2.9758908919249000e-03 7.3389857579700624e-02 3.4651814800093540e+01 1.7560069564760610e+01 1.3634687138200170e+00 0 0 """
     parts = line.split()
+    f = PdfInfo()
+    return f
+
 
 def parseGenVertexLine(line):
     """Parse line from HepMC file containing GenVertex info
     e.g. V -3 0 0 0 0 0 0 2 0 """
-    pass
+    parts = line.split()
+    v = GenVertex()
+    return v
 
 
 def parseGenParticleLine(line):
     """Parse line from HepMC file containing GenParticle info
     e.g. P 4 21 0 0 -2.9355943031880248e+05 2.9355943031880248e+05 0 21 0 0 -3 2 1 102 2 103 """
-    pass
+    parts = line.split()
+    p = GenParticle()
+    return p
