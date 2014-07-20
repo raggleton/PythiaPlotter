a317c611bf36d0dd2914ca273f37f008ccec5a03
Robin Aggleton <robinaggleton@gmail.com>
2014-07-20 19:10:40 +0100

    Add function placeholders for lines

--- hepmcReader.py
+++ hepmcReader.py
@@ -1,6 +1,6 @@
 """
     Set of method(s) to interpret hepmc file.
-    
+
     See hepmcformat.txt for reminder, or section 6 of hepMC2 user manual
 """
 
@@ -8,13 +8,14 @@
 
 verbose = True
 
+
 def parse(fileName="testSS_HLT.hepmc"):
     """Parse HepMCfile and return collection of events"""
 
     print "Parsing events from ", fileName
-    
+
     with open(fileName, 'r') as file:
-        
+
         for line in file:
             if verbose: print line,
 
@@ -27,6 +28,58 @@ def parse(fileName="testSS_HLT.hepmc"):
             if line.startswith("HepMC::IO_GenEvent-START_EVENT_LISTING"):
                 print "Start parsing event listing"
 
+            # general GenEvent information            
+            if line.startswith("E"):
+                genEvent = parseGenEventLine(line)
+
+            # named weights
+            if line.startswith("N"):
+                weights = parseWeightsLine(line)
+
+            # momentum and position units
+            if line.startswith("U"):
+                units = parseUnitsLine(line)
+
+            # GenCrossSection information: 
+            # This line will appear ONLY if GenCrossSection is defined.
+            if line.startswith("C"):
+                genCrossSection = parseCrossSectionLine(line)
+
+            # HeavyIon information: 
+            # This line will contain zeros if there is no associated 
+            # HeavyIon object. 
+            # We don't use this so ignore (for now)
+            if line.startswith("H"):
+                print "We don't deal with this"
+                pass
+
+            # PdfInfo information: 
+            # This line will contain zeros if there is no associated PdfInfo object.
+            if line.startswith("F"):
+                pdfInfo = parsePdfInfoLine(line)
+
             # Get end of event block
             if line.startswith("HepMC::IO_GenEvent-END_EVENT_LISTING"):
                 print "End parsing event listing"
+
+
+def parseGenEventLine(line):
+    # Parse line from HepMC file containting GenEvent info
+
+    
+def parseWeightsLine(line):
+    # Parse line from HepMC file containting Weights info
+
+    
+def parseUnitsLine(line):
+    # Parse line from HepMC file containting Units info
+
+    
+def parseCrossSectionLine(line):
+    # Parse line from HepMC file containting CrossSection info
+
+    
+def parsePdfInfoLine(line):
+    # Parse line from HepMC file containting PdfInfo info
+
+    
