3fc84bda16d01474c406a3fbbb75f0eccaf8bd5b
Robin Aggleton <robinaggleton@gmail.com>
2014-07-29 17:03:33 +0100

    Update test template for relative import

--- test/test_template.py
+++ test/test_template.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
 import unittest
+# To import from directory above test/
+# Doing:
+# from .. import eventclasses
+# doens't work as doens't think it's a package
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+import eventClasses
 
 class FooTests(unittest.TestCase):
 
