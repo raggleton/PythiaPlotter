9ee29818be78e0a88b783c6bca059b5e0ec011de
Robin Aggleton <robinaggleton@gmail.com>
2015-02-03 11:37:00 +0100

    slight restructure of requisite checker, and add in overall check

--- requisite_checker.py
+++ requisite_checker.py
@@ -9,46 +9,6 @@ import subprocess
 from distutils.spawn import find_executable
 
 
-class RequisiteChecker():
-    """
-    Class to handle checking of modules/programs.
-    Queryable for program/module name via the results dictionary.
-    """
-
-    def __init__(self, modules=None, programs=None):
-        modules = [] if not modules else modules
-        programs = [] if not programs else programs
-        self.results = dict()
-
-        for mod in modules:
-            self.results[mod] = check_module_exists(mod)
-        for prog in programs:
-            self.results[prog] = check_program_exists(prog)
-
-
-def check_program_exists_old(program):
-    """Test if external program runs."""
-    # Old, not so great version that relied on program having a -h option
-    try:
-        # Storing in string stifles output
-        prog_out = subprocess.check_output([program, "-h"],
-                                           stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as cpe:
-        print(cpe.returncode)
-        print(cpe.output)
-        return False
-    except OSError as e:
-        if e.errno == os.errno.ENOENT:
-            print "!!! You need to install program \"" + program + \
-                  "\" or add it to PATH variable"
-            print(e)
-        else:
-            # Something else went wrong while trying to run
-            print(e)
-        return False
-    return True
-
-
 def check_program_exists(program):
     """Test if external program runs."""
     if find_executable(program):
@@ -67,3 +27,45 @@ def check_module_exists(module):
         print "!!! Module \"" + module + "\" doesn't exist"
         return False
     return True
+
+
+class RequisiteChecker():
+    """
+    Class to handle checking of modules/programs.
+    Queryable for program/module name via the results dictionary.
+    Or to check all exist, use self.all_exist.
+    """
+
+    def __init__(self, modules=None, programs=None):
+        modules = [] if not modules else modules
+        programs = [] if not programs else programs
+
+        self.results = dict()
+        for mod in modules:
+            self.results[mod] = check_module_exists(mod)
+        for prog in programs:
+            self.results[prog] = check_program_exists(prog)
+
+        self.all_exist = all(list(self.results.values()))
+
+# def check_program_exists_old(program):
+#     """Test if external program runs."""
+#     # Old, not so great version that relied on program having a -h option
+#     try:
+#         # Storing in string stifles output
+#         prog_out = subprocess.check_output([program, "-h"],
+#                                            stderr=subprocess.STDOUT)
+#     except subprocess.CalledProcessError as cpe:
+#         print(cpe.returncode)
+#         print(cpe.output)
+#         return False
+#     except OSError as e:
+#         if e.errno == os.errno.ENOENT:
+#             print "!!! You need to install program \"" + program + \
+#                   "\" or add it to PATH variable"
+#             print(e)
+#         else:
+#             # Something else went wrong while trying to run
+#             print(e)
+#         return False
+#     return True
