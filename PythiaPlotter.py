fa03ea08a2356231f1e4673d9bf73f401a4862f7
Robin Aggleton <robinaggleton@gmail.com>
2015-02-17 14:26:13 +0100

    Start to flesh out main script a bit more

--- PythiaPlotter.py
+++ PythiaPlotter.py
@@ -2,21 +2,39 @@
 Main script to run PythiaPlotter.
 """
 
-import helper_methods as helper
-from requisite_checker import RequisiteChecker
 import user_args
+
 
 class PythiaPlotter():
     """
     Store central objects & args used
     """
-    def __init__(self):
+    def __init__(self, opts):
+        self.opts = opts
         self.parser = None
         self.printer = None
         self.gen_event = None
 
-    def parse_event(self, event):
-        # return self.parser.parse(event)
+        # Choose parser
+        if opts.inputFormat == "PYTHIA":
+            self.parser = PythiaParser(opts.input, opts.eventNumber)
+        elif opts.inputFormat == "HEPMC":
+            pass
+
+        # Choose printer
+        if opts.render == "DOT":
+            pass
+        elif opts.render == "LATEX":
+            pass
+
+    def __repr__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def parse_event(self, thingtoparse, event_num):
+        # return self.parser.parse(thingtoparse)
         pass
 
     def print_event(self, gen_event):
@@ -24,15 +42,15 @@ class PythiaPlotter():
         pass
 
 
-if __name__ == "__main__":
-
-    # Parse args
-    opts = user_args.get_args()
-
+def main():
     # Central Pythia_Plotter object to keep track of the parser, printer, etc
-    pp = PythiaPlotter()
+    pp = PythiaPlotter(user_args.get_args())
 
     # for event in event_list:
-    event = None
-    gen_event = pp.parse_event(event)
+    thingtoparse = None
+    gen_event = pp.parse_event(thingtoparse)
     pp.print_event(gen_event)
+
+
+if __name__ == "__main__":
+    main()
