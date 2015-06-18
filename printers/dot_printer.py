8773e4373c73feddd5e86118843750ba7f59db70
Robin Aggleton <robinaggleton@gmail.com>
2015-06-18 23:56:42 +0200

    minor change to the way event label is handled, with extra debug statments

--- printers/dot_printer.py
+++ printers/dot_printer.py
@@ -43,10 +43,11 @@ class DotPrinter(object):
         graph.graph["attr"] = DotGraphAttr(graph)
 
         for node_ind in graph.nodes():
-            node = graph.node[node_ind]
-            node["attr"] = DotNodeAttr(node)
+            graph.node[node_ind]["attr"] = DotNodeAttr(graph.node[node_ind])
 
         for edges_ind in graph.edges():
+            # NB if graph is a MultiDiGraph then need to add extra index [0]
+            # since could have multiple edge between same vertices
             edge = graph.edge[edges_ind[0]][edges_ind[1]]
             edge["attr"] = DotEdgeAttr(edge)
 
@@ -58,22 +59,26 @@ class DotPrinter(object):
 
             graph = event.graph
 
+            log.debug("At the printers:")
+            log.debug(graph.edges())
+
             # Header-type info with graph-wide settings
             dot_file.write("digraph g {\n")
-            dot_file.write("\t{attr}\n".format(**graph.graph))
+            dot_file.write("{attr}\n".format(**graph.graph))
 
             # Add event info to plot
+            lbl = ""
             if event.label:
                 # Event title
                 lbl = "<FONT POINT-SIZE=\"45\"><B>{0}</B></FONT><BR/>".format(event.label)
-                lbl += "<FONT POINT-SIZE=\"40\">  <BR/>"
-                # Event info
-                # Keep event.label as a title, not in attribute list
-                evt_lbl = [x for x in event.__str__().split("\n")
-                           if not (x.startswith("label") or x.startswith("Event"))]
-                lbl += '<BR/>'.join(evt_lbl)
-                lbl += "</FONT>"
-                dot_file.write("\tlabel=<{0}>;\n".format(lbl))
+            lbl += "<FONT POINT-SIZE=\"40\">  <BR/>"
+            # Event info
+            # Keep event.label as a title, not in attribute list
+            evt_lbl = [x for x in event.__str__().split("\n")
+                       if not (x.startswith("label") or x.startswith("Event"))]
+            lbl += '<BR/>'.join(evt_lbl)
+            lbl += "</FONT>"
+            dot_file.write("\tlabel=<{0}>;\n".format(lbl))
 
             # Now print the graph to file
 
