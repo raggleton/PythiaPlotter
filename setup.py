fc6561e63425e8ceeeced6df3d7fbde0f95c757d
Robin Aggleton <robin.aggleton@cern.ch>
2017-01-12 13:25:09 +0000

    Add in a basic setup.py for pip-able installation

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PythiaPlotter",
+    version="0.2.0",
+    description="Generate diagram of particles in the event tree from a "
+                "High-Energy Physics Monte Carlo event generator.",
+    long_description="SOMETHING HERE",
+    author="Robin Aggleton",
+    author_email="robinaggleton@gmail.com",
+    url="https://github.com/raggleton/PythiaPlotter",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'PythiaPlotter=PythiaPlotter:main'
+        ]
+    }
+)
