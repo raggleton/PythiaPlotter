3bd9c8eee3ee48cccddbbb1f4aaf96452eb0ad68
Robin Aggleton <robin.aggleton@cern.ch>
2017-01-15 17:20:56 +0000

    Move away from PythiaPlotter class, and just use functions for each step.
    Not perfect, but little steps on the road to refactor wellness...

--- pythiaplotter/PythiaPlotter.py
+++ pythiaplotter/PythiaPlotter.py
@@ -17,71 +17,59 @@ from pythiaplotter.utils.common import open_pdf
 log = logging.getLogger(__name__)
 
 
-class PythiaPlotter(object):
-    """Central object to keep track of the parser, printer, etc"""
+def choose_parser(opts):
+    """Choose parser & configure"""
 
-    def __init__(self, opts):
-        self.opts = opts
-        self.parser = None
-        self.printer = None
-        self.event = None
+    remove_redundants = not opts.redundants
 
-        remove_redundants = not opts.redundants
-        # Choose parser & configure
-        if opts.inputFormat == "PYTHIA":
-            self.parser = parsers.Pythia8Parser(filename=opts.input,
-                                                event_num=opts.eventNumber,
-                                                remove_redundants=remove_redundants)
-        elif opts.inputFormat == "HEPMC":
-            # For now, disable remove_redundants for HepMC until it gets fixed
-            log.warning("Disabling removal of redundants for HepMC files as broken")
-            self.parser = parsers.HepMCParser(filename=opts.input,
-                                              event_num=opts.eventNumber,
-                                              # remove_redundants=remove_redundants)
-                                              remove_redundants=False)
-        elif opts.inputFormat == "LHE":
-            self.parser = parsers.LHEParser(filename=opts.input,
-                                            event_num=opts.eventNumber,
-                                            remove_redundants=remove_redundants)
-        elif opts.inputFormat == "CMSSW":
-            self.parser = parsers.CMSSWParticleListParser(filename=opts.input,
-                                                          remove_redundants=remove_redundants)
-
-        # Choose printer & configure
-        if opts.render == "DOT":
-            self.printer = printers.DotPrinter(gv_filename=opts.outputGV,
-                                               pdf_filename=opts.outputPDF,
-                                               renderer="dot",
-                                               output_format="pdf")
-        elif opts.render == "LATEX":
-            pass
-
-    def __repr__(self):
-        pass
-
-    def __str__(self):
-        pass
-
-    def parse_event(self):
-        """Run the object's parser"""
-        self.event = self.parser.parse()
-        if self.opts.stats:
-            self.event.print_stats()
-
-    def print_event(self):
-        """Run the object's printer"""
-        self.printer.print_event(self.event, pdf=(not self.opts.noPDF))
-        if self.opts.openPDF:
-            open_pdf(self.opts.outputPDF)
+    if opts.inputFormat == "PYTHIA":
+        return parsers.Pythia8Parser(filename=opts.input,
+                                     event_num=opts.eventNumber,
+                                     remove_redundants=remove_redundants)
+    elif opts.inputFormat == "HEPMC":
+        # For now, disable remove_redundants for HepMC until it gets fixed
+        log.warning("Disabling removal of redundants for HepMC files as broken")
+        return parsers.HepMCParser(filename=opts.input,
+                                   event_num=opts.eventNumber,
+                                   # remove_redundants=remove_redundants)
+                                   remove_redundants=False)
+    elif opts.inputFormat == "LHE":
+        return parsers.LHEParser(filename=opts.input,
+                                 event_num=opts.eventNumber,
+                                 remove_redundants=remove_redundants)
+    elif opts.inputFormat == "CMSSW":
+        return parsers.CMSSWParticleListParser(filename=opts.input,
+                                               remove_redundants=remove_redundants)
+    else:
+        return None
 
 
-def main(args=sys.argv[1:]):
-    pp = PythiaPlotter(cli.get_args(args))
-    pp.parse_event()
-    pp.event.label = "Alex's event in MG5_aMC@NLO"
-    pp.event.event_num = pp.opts.eventNumber
+def choose_printer(opts):
+    """Choose printer & configure"""
+
+    if opts.render == "DOT":
+        return printers.DotPrinter(gv_filename=opts.outputGV,
+                                   pdf_filename=opts.outputPDF,
+                                   renderer="dot",
+                                   output_format="pdf")
+    elif opts.render == "LATEX":
+        return None
+
+
+def main(in_args=None):
+    opts = cli.get_args(in_args)
+    parser = choose_parser(opts)
+    event = parser.parse()
+    if opts.stats:
+        event.print_stats()
+    event.label = "Alex's event in MG5_aMC@NLO"
+    event.event_num = opts.eventNumber
     # pp.event.lumi_section = 123456798
-    pp.print_event()
+    printer = choose_printer(opts)
+    printer.print_event(event, make_pdf=(not opts.noPDF))
+    if opts.openPDF:
+        open_pdf(opts.outputPDF)
+    return 0
 
 
 if __name__ == "__main__":
