0eb57743a1dc94a0b1cbd600cd125e4b8262659a
Robin Aggleton <robin.aggleton@cern.ch>
2017-01-25 18:50:31 +0000

    Make test_examples_full.py work with any input args, and handle lack of PyROOT properly using pytest

--- tests/test_examples_full.py
+++ tests/test_examples_full.py
@@ -1,45 +1,36 @@
-"""
-These tests are designed to run the program in full using the included examples.
-"""
+""" These tests are designed to run the program in full using the included examples."""
 
 
 from __future__ import absolute_import
-import os
 from pythiaplotter.PythiaPlotter import main
-
-
-class RunOptions(object):
-    """Class to store and inject options for a run of the program"""
-
-
-    # TODO: design this better, very basic atm
-    def __init__(self, input_filename, output_pdf, input_format):
-        self.input_filename = input_filename
-        self.output_pdf = output_pdf
-        self.input_format = input_format
-
-    def run_opts(self):
-        result = main([self.input_filename, '-O', self.output_pdf, '--inputFormat', self.input_format])
-        if result == 0:
-            os.remove(self.output_pdf)
-            os.remove(self.output_pdf.replace('.pdf', '.gv'))
+import pytest
 
 
 def test_run_py8():
-    r = RunOptions("example/example_pythia8.txt", 'test_py8.pdf', 'PYTHIA')
-    r.run_opts()
+    main(["example/example_pythia8.txt", '-O', 'test_py8.pdf', '--inputFormat', 'PYTHIA'])
 
 
 def test_run_hepmc():
-    r = RunOptions("example/example_hepmc.hepmc", 'test_hepmc.pdf', 'HEPMC')
-    r.run_opts()
+    main(["example/example_hepmc.hepmc", '-O', 'test_hepmc.pdf', '--inputFormat', 'HEPMC'])
 
 
 def test_run_cmssw():
-    r = RunOptions("example/example_cmssw.txt", 'test_cmssw.pdf', 'CMSSW')
-    r.run_opts()
+    main(["example/example_cmssw.txt", '-O', 'test_cmssw.pdf', '--inputFormat', 'CMSSW'])
 
 
 def test_run_lhe():
-    r = RunOptions("example/example_lhe.lhe", 'test_lhe.pdf', 'LHE')
-    r.run_opts()
+    main(["example/example_lhe.lhe", '-O', 'test_lhe.pdf', '--inputFormat', 'LHE'])
+
+
+def test_run_lhe_ps():
+    main(["example/example_lhe.lhe", '-O', 'test_lhe_ps.pdf', '--inputFormat', 'LHE',
+          '--outputFormat', 'ps'])
+
+
+def test_run_lhe_jpeg():
+    main(["example/example_lhe.lhe", '-O', 'test_lhe.jpg', '--inputFormat', 'LHE'])
+
+
+def test_run_heppy():
+    pyroot = pytest.importorskip("ROOT")
+    main(["example/example_heppy.root", '-O', "test_heppy.pdf", '--inputFormat', "HEPPY"])
