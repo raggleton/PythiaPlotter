34b7073d7a147c4267ec011b9fcd2f50e63c082b
Robin Aggleton <robin.aggleton@cern.ch>
2017-01-29 19:07:31 +0000

    Add more full test examples, and choice to run get_args() or main()

--- tests/test_examples_full.py
+++ tests/test_examples_full.py
@@ -1,55 +1,68 @@
-""" These tests are designed to run the program in full using the included examples."""
+"""These tests are designed to run the program either in full using the included examples, or just parsing args."""
 
 
 from __future__ import absolute_import
-from pythiaplotter.PythiaPlotter import main
 import pytest
+from pythiaplotter.PythiaPlotter import main
+from pythiaplotter.cli import get_args
 
 
-def test_run_py8():
-    main(["example/example_pythia8.txt", '-O', 'test_py8.pdf', '--inputFormat', 'PYTHIA', '--stats'])
+FUNC = get_args
 
 
-def test_run_py8_redundants():
-    main(["example/example_pythia8.txt", '-O', 'test_py8.pdf', '--inputFormat', 'PYTHIA', '--redundants'])
+@pytest.mark.xfail
+def test_run_no_input(main_func=FUNC):
+    main_func(['--inputFormat', 'PYTHIA'])
 
 
-def test_run_py8_convert():
-    main(["example/example_pythia8.txt", '-O', 'test_py8_conv.pdf', '--inputFormat', 'PYTHIA', '-r', 'EDGE'])
+def test_run_py8_minimal(main_func=FUNC):
+    main_func(["example/example_pythia8.txt", '--inputFormat', 'PYTHIA', '--stats'])
 
 
-def test_run_py8_sfdp():
-    main(["example/example_pythia8.txt", '-O', 'test_py8_conv.pdf', '--inputFormat', 'PYTHIA', '--layout', 'sfdp'])
+def test_run_py8_redundants(main_func=FUNC):
+    main_func(["example/example_pythia8.txt", '-O', 'test_py8.pdf', '--inputFormat', 'PYTHIA', '--redundants'])
 
 
-def test_run_hepmc():
-    main(["example/example_hepmc.hepmc", '-O', 'test_hepmc.pdf', '--inputFormat', 'HEPMC'])
+def test_run_py8_ofmt(main_func=FUNC):
+    main_func(["example/example_pythia8.txt", '--outputFormat', 'pdf', '--inputFormat', 'PYTHIA', '--redundants'])
 
 
-def test_run_hepmc_redundants():
-    main(["example/example_hepmc.hepmc", '-O', 'test_hepmc.pdf', '--inputFormat', 'HEPMC', '--redundants'])
+def test_run_py8_convert(main_func=FUNC):
+    main_func(["example/example_pythia8.txt", '-O', 'test_py8_conv.pdf', '--inputFormat', 'PYTHIA', '-r', 'EDGE'])
 
 
-def test_run_hepmc_convert():
-    main(["example/example_hepmc.hepmc", '-O', 'test_hepmc_conv.pdf', '--inputFormat', 'HEPMC', '-r', 'NODE'])
+def test_run_py8_sfdp(main_func=FUNC):
+    main_func(["example/example_pythia8.txt", '-O', 'test_py8_conv.pdf', '--inputFormat', 'PYTHIA', '--layout', 'sfdp'])
 
 
-def test_run_cmssw():
-    main(["example/example_cmssw.txt", '-O', 'test_cmssw.pdf', '--inputFormat', 'CMSSW'])
+def test_run_hepmc(main_func=FUNC):
+    main_func(["example/example_hepmc.hepmc", '-O', 'test_hepmc.pdf', '--inputFormat', 'HEPMC'])
 
 
-def test_run_lhe():
-    main(["example/example_lhe.lhe", '-O', 'test_lhe.pdf', '--inputFormat', 'LHE'])
+def test_run_hepmc_guess_input(main_func=FUNC):
+    main_func(["example/example_hepmc.hepmc", '-O', 'test_hepmc.pdf'])
 
 
-def test_run_lhe_ps():
-    main(["example/example_lhe.lhe", '-O', 'test_lhe_ps.pdf', '--inputFormat', 'LHE', '--outputFormat', 'ps'])
+def test_run_hepmc_redundants(main_func=FUNC):
+    main_func(["example/example_hepmc.hepmc", '-O', 'test_hepmc.pdf', '--inputFormat', 'HEPMC', '--redundants'])
 
 
-def test_run_lhe_jpeg():
-    main(["example/example_lhe.lhe", '-O', 'test_lhe.jpg', '--inputFormat', 'LHE'])
+def test_run_hepmc_convert(main_func=FUNC):
+    main_func(["example/example_hepmc.hepmc", '-O', 'test_hepmc_conv.pdf', '--inputFormat', 'HEPMC', '-r', 'NODE'])
 
 
-def test_run_heppy():
+def test_run_cmssw(main_func=FUNC):
+    main_func(["example/example_cmssw.txt", '-O', 'test_cmssw.pdf', '--inputFormat', 'CMSSW'])
+
+
+def test_run_lhe(main_func=FUNC):
+    main_func(["example/example_lhe.lhe", '-O', 'test_lhe.pdf', '--inputFormat', 'LHE'])
+
+
+def test_run_lhe_ps(main_func=FUNC):
+    main_func(["example/example_lhe.lhe", '-O', 'test_lhe_ps.pdf', '--inputFormat', 'LHE', '--outputFormat', 'ps'])
+
+
+def test_run_heppy(main_func=FUNC):
     pytest.importorskip("ROOT")
-    main(["example/example_heppy.root", '-O', "test_heppy.pdf", '--inputFormat', "HEPPY"])
+    main_func(["example/example_heppy.root", '-O', "test_heppy.pdf", '--inputFormat', "HEPPY"])
